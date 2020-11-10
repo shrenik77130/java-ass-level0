18c69d28f4bf98054ebef84941312e2ddb7febc5
shrenik77130 <shrenik77130@gmail.com>
2020-11-10 19:20:27 +0530

    initial commit

---
+++ Q1.java
@@ -0,0 +1,21 @@
+
+class Q1
+{
+	public static void main(String []args)
+	{
+		// input width & height
+
+		calcArea();
+		calcPerimeter();
+	}
+
+	public void calcArea()
+	{
+
+	}
+
+	public void calcPerimeter()
+	{
+
+	}
+}
